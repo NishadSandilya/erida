9ab406a85b87520451f7d06ce95f668e383272a4
Nishad Sandilya <67221201+NishadSandilya@users.noreply.github.com>
2021-08-23 19:38:29 +0530

    initialCommit2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/app.css'
-
+import {HashRouter as Router} from 'react-router-dom'
 //Render the component
 ReactDOM.render(
-    <App/>
+    <Router basename='/'>
+        <App/>
+    </Router>
 ,document.getElementById('root'))
