8f767f5d1cebee051bd407e77a7abb918c234c6d
Nishad Sandilya <67221201+NishadSandilya@users.noreply.github.com>
2021-10-02 18:43:38 +0530

    fixing razorpay

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -58,7 +58,7 @@ const OrderCard = ({ orderDetails }) => {
                     order_id: serverResponse?.data?.payload?.razorpayOrder?.id,
                     prefill: serverResponse?.data?.payload?.prefill,
                     description: "Thank you for choosing erida",
-                    callback_url: "https://erida.in/orders"
+                    callback_url: "https://erida.in"
                 }
 
                 //Finally call the sdk
